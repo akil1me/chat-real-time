44e70748a11e7195d92074fe9fecb99d3357e743
akil1me <akipax123@gmail.com>
2022-12-25 21:52:06 +0500

    add style in login page

---
+++ src/components/login-google/login-google.styled.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const LoginAcc = styled.div`
+ display: flex;
+  justify-content:center;
+  align-items:center;
+  height: 80vh; 
+ 
+`
+
+export const LoginGoogleContent = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color:#fff;
+  border-radius:10px;
+  padding:50px;
+
+&> h2{
+  font-size: 20px;
+  margin-bottom: 10px;
+  text-align:center;
+}
+@media only screen and (max-width: 323px) {
+   padding:10px;
+  }
+
+`
