5b8ffab4f86674073bf9628d0b49d86a0f447a39
akil1me <akipax123@gmail.com>
2022-12-27 15:38:21 +0500

    updeted navbar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,22 +1,26 @@
 import { Button, Col, Row } from "antd"
 import { signOut } from "firebase/auth"
 import { useAuthState } from "react-firebase-hooks/auth"
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/img/logo.svg"
 import { auth } from "../../utils/firebase"
 
 export const NavBar = ({ loginGoogle }) => {
   const [user] = useAuthState(auth);
+  console.log(user);
 
   return (
     <nav>
-      <Row justify={"space-between"}>
+      <Row justify="space-between" align="middle">
         <Col>
-          <a href="index.html">
-            <img src={logo} alt="site logo" width={40} height={40} />
-          </a>
+          <Link to="index.html">
+            <img style={{ borderRadius: "50%" }} src={user ? user.photoURL : logo} alt="site logo" width={40} height={40} />
+          </Link>
         </Col>
-
+        {
+          user && <Col> <span>Welcome, {user.displayName}</span> </Col>
+        }
         <Col>
           {user ?
             <Button onClick={() => signOut(auth)}>Logout</Button>
