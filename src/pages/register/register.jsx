8f77f7a9f4cb370ac9ef19e8a26f03c39e8398a8
akil1me <akipax123@gmail.com>
2023-02-22 02:47:00 +0500

    fix register bug

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -10,21 +10,22 @@ import { auth, db, storage } from "../../utils/firebase";
 
 const Header = styled.header`
   background-color: #bae7ff;
-`
+`;
 export const Register = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmitRegister = async (email, password, displayName, file,) => {
-    setLoading(true)
+  const handleSubmitRegister = async (email, password, displayName, file) => {
+    setLoading(true);
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password)
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-      const storageRef = ref(storage, new Date().getTime());
+      const storageRef = ref(storage, String(new Date().getTime()));
 
       const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on("state_changed",
-        () => { },
+      uploadTask.on(
+        "state_changed",
+        () => {},
         (error) => {
           console.log(error);
         },
@@ -33,32 +34,28 @@ export const Register = () => {
             try {
               await updateProfile(res.user, {
                 displayName,
-                photoURL: downloadURL
-              })
+                photoURL: downloadURL,
+              });
 
               await setDoc(doc(db, "users", res.user.uid), {
                 uid: res.user.uid,
                 displayName,
                 photoURL: downloadURL,
                 email,
-              })
-            }
-            catch (err) {
-              message.error(err)
-            }
-            finally {
-              setLoading(false)
+              });
+            } catch (err) {
+              message.error(err);
+            } finally {
+              setLoading(false);
             }
           });
         }
       );
-    }
-    catch (err) {
-      message.error(err.code)
+    } catch (err) {
+      message.error(err.code);
       console.log(err);
     }
-  }
-
+  };
 
   return (
     <>
@@ -77,7 +74,8 @@ export const Register = () => {
             <Form.Item
               style={{ maxWidth: 218 }}
               name={"file"}
-              rules={[{ required: true, message: "Please confirm your image" }]}>
+              rules={[{ required: true, message: "Please confirm your image" }]}
+            >
               <Upload
                 name="file"
                 listType={"picture"}
@@ -92,24 +90,27 @@ export const Register = () => {
           confirm={
             <Form.Item
               name="confirm"
-              dependencies={['password']}
+              dependencies={["password"]}
               validateTrigger={"onChange"}
               rules={[
                 {
                   required: true,
-                  message: 'Please confirm your password!',
+                  message: "Please confirm your password!",
                 },
                 ({ getFieldValue }) => ({
                   validator(_, value) {
-                    if (!value || getFieldValue('password') === value) {
+                    if (!value || getFieldValue("password") === value) {
                       return Promise.resolve();
                     }
-                    return Promise.reject(new Error('No password matches'));
+                    return Promise.reject(new Error("No password matches"));
                   },
                 }),
               ]}
             >
-              <Input.Password prefix={<LockOutlined />} placeholder="Confirm password" />
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Confirm password"
+              />
             </Form.Item>
           }
           userName={
@@ -118,14 +119,15 @@ export const Register = () => {
               rules={[
                 {
                   required: true,
-                  message: 'Please input your UserName!',
+                  message: "Please input your UserName!",
                 },
               ]}
             >
               <Input prefix={<UserOutlined />} placeholder="username" />
             </Form.Item>
-          } />
+          }
+        />
       </Container>
     </>
-  )
-}
+  );
+};
